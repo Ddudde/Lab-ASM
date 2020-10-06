796879043838f845a2fb7e1ea8cec0831d818f19
Dude <BAX.152MM@yandex.ru>
2020-10-06 11:31:03 +0300

    Dop#1

---
+++ Lab1/Chet.asm
@@ -0,0 +1,24 @@
+%include "io.inc"
+
+
+section .data
+    chot db "Чётное число", 0
+    nechot db "Нечётное число", 0
+
+section .text
+global CMAIN
+CMAIN:
+    mov ebp, esp; for correct debugging
+    GET_DEC 4, eax
+    ;mov ax, 2
+    test eax, 1
+    jnz no
+    jz yes
+yes:
+    PRINT_STRING chot
+    NEWLINE
+no:
+    PRINT_STRING nechot
+    NEWLINE
+    xor eax, eax
+    ret
