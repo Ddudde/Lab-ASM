8b97fb091679b673872e94abb716623125a3c43d
Dude <BAX.152MM@yandex.ru>
2020-10-07 07:07:59 +0300

    Dop#2

---
+++ Lab2/А.asm
@@ -0,0 +1,37 @@
+%include "io.inc"
+
+section .bss
+    N equ 5
+
+section .text
+global CMAIN
+CMAIN:
+    mov ecx, N
+    lea esi, [mass]
+    input:
+        lodsd
+        PRINT_DEC   4, eax
+        PRINT_CHAR  ' '
+        add esi, 4
+    loop input
+    NEWLINE
+    PRINT_DEC 4, eax
+    ret
+    ;mov ecx, N
+    ;mov eax, summ
+    ;lea esi,[mass+4]
+    ;mov eax,0
+    ;input:
+    ;    mov ebx,[esi]
+    ;    add eax, ebx
+    ;    PRINT_DEC 4, ebx
+    ;    ADD esi,4
+    ;loop input
+    ;NEWLINE
+    ;PRINT_DEC 4, eax
+    ;ret
+    
+    
+section .data
+    mass dd 5,4,-6,3,6
+    summ    dd 0
