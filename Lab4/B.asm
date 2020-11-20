d1a57f979adeffa0294f05fa7a7de5f5de50e757
Ddudde <BAX.152MM@yandex.ru>
2020-11-20 14:50:00 +0300

    Dop#4

--- Lab4/B.asm
+++ Lab4/B.asm
@@ -3,24 +3,16 @@
 section .text
 global CMAIN
 CMAIN:
-    mov edx, 2
-    cmp edx, 2
-    je L1
-    ja L4
-    L1: mov ax, [xb]
-        mov bx, [yb]
-        mov cx, [zb]
-        call min
-        ret
-    L4: mov ebx, [x]
-        mov eax, [y]
-        mov ecx, [z]
-        call min
-        ret
+    mov ebx, [x]
+    mov eax, [y]
+    mov ecx, [z]
+    mov edx, 2 ;тип
+    call min
+    ret
     min:
         cmp edx, 2
         je L8
-        ja L7
+        jne L7
     L7: cmp eax, ecx
         jl L2
         mov eax, ebx
@@ -28,7 +20,9 @@ CMAIN:
         jl L3
         mov eax, ecx
     L3: mov [rez], eax
+        PRINT_STRING 'ћинимальное число: '
         PRINT_DEC 4, rez
+        PRINT_STRING ', количество байт в числе: 4'
         ret
     L8: cmp ax, bx
         jl L5
@@ -37,16 +31,14 @@ CMAIN:
         jl L6
         mov ax, bx
     L6: mov [rezb], ax
-        PRINT_STRING ' оличество байт в числе: '
+        PRINT_STRING 'ћинимальное число: '
         PRINT_DEC 2, rezb
+        PRINT_STRING ', количество байт в числе: 2'
     ret
 
 section .data
     x dd 3
     y dd -5
     z dd 3
-    xb dd 4
-    yb dd 4
-    zb dd 4
     rez dd 0
     rezb dd 0
